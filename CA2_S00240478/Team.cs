d12fbd4a22d0f700bfba93b10447b70d42450b8c
dvarava <s00240478@atu.ie>
2023-12-14 10:52:41 +0000

    Created Team class

---
+++ CA2_S00240478/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_S00240478
+{
+    internal class Team
+    {
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
